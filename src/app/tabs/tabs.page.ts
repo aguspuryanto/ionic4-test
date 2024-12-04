d03902e7e7313b12c55ad2bf9fc01654e0364978
Agus Puryanto <aguspuryanto@gmail.com>
2024-12-04 09:49:59 +0700

    update

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,39 +1,28 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-=======
 // IonIcon 
 import { IonIcon } from '@ionic/angular';
 // addIcons 
 import { addIcons } from 'ionicons';
->>>>>>> 54b23ecb75af3ee1f22092c8866d0a026803d01d
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-<<<<<<< HEAD
-  styleUrls: ['tabs.page.scss']
-})
-export class TabsPage {
-
-  constructor() {}
-=======
   styleUrls: ['tabs.page.scss'],
   standalone: false,
   // imports: [IonIcon]
 })
 export class TabsPage {
 
-  aws = './assets/images/aws_000.png';
-  statistics = './assets/images/statistics.png';
-  info = './assets/images/info.png';
+  awsImg = '/assets/images/aws_000.png';
+  statImg = '/assets/images/statistics.png';
+  infoImg = '/assets/images/info.png';
 
   constructor() {
     addIcons({
-      'aws': 'assets/images/aws_000.png',
-      'statistics': 'assets/images/statistics.png',
-      'info': 'assets/images/info.png'
+      'aws': '/assets/images/aws_000.png',
+      'statistics': '/assets/images/statistics.png',
+      'info': '/assets/images/info.png'
     });
   }
->>>>>>> 54b23ecb75af3ee1f22092c8866d0a026803d01d
 
 }
